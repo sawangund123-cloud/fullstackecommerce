4a6b033ca8899f84df8530cfa2e292caad3c8996
sawan gund <sawangund123@gmail.com>
2026-06-25 11:06:21 +0530

    first commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,17 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const protect = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+  
+   {
+    try {  
+      const token = req.headers.authorization;
+      
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+      
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // decoded stores id in it
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
@@ -14,9 +20,6 @@ const protect = async (req, res, next) => {
     }
   }
 
-  if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
-  }
 };
 
 module.exports = { protect };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,14 @@ const { protect } = require('../middleware/authMiddleware');
 const { admin } = require('../middleware/adminMiddleware');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+// multer helps in processing image , pds ,videos which express cant do and the image processed can be get by req.file which gives file name and apth whi ch can be uploaded in cloudinary
 
 const router = express.Router();
 
-router.route('/').get(getProducts).post(protect, admin, upload.single('image'), createProduct);
+
+router.get('/',getProducts);
+router.post("/",protect, admin, upload.single('image'), createProduct);
+
 router.route('/:id').get(getProductById).put(protect, admin, upload.single('image'), updateProduct).delete(protect, admin, deleteProduct);
 
 module.exports = router;
